32a5c1dd42ab9d881cb00c50ba4719ecda80c623
We2Am-BaSsem <58189568+We2Am-BaSsem@users.noreply.github.com>
2020-12-24 22:48:31 +0200

    Update Delivery.py

--- BackEnd/Delivery.py
+++ BackEnd/Delivery.py
@@ -3,14 +3,14 @@ from flask import request
 from SQLAPI import SQL
 # ------------------------------------------------------------------------------------------------------------------------------
 # Connection Arguments of the database
-#server_name = "dentista1.mysql.database.azure.com"
-#server_admin = "dentista@dentista1"
-#server_password = "Itachionly#1"
-#database = "DENTISTA"
-server_name = "127.0.0.1"
-server_admin = "root"
+server_name = "dentista1.mysql.database.azure.com"
+server_admin = "dentista@dentista1"
 server_password = "Itachionly#1"
-database = "dantista"
+database = "DENTISTA"
+#server_name = "127.0.0.1"
+#server_admin = "root"
+#server_password = "Itachionly#1"
+#database = "dantista"
 connection_details = [server_name, server_admin, server_password, database]
 # --------------------------------------------------------------------------------------------------------------------------------
 
