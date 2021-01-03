e20aadddd2d3cd162af54df084b93f7ca4d04887
YousefGamal220 <57014132+YousefGamal220@users.noreply.github.com>
2021-01-03 16:40:34 +0200

    Update Login_Auth.py

--- BackEnd/Login_Auth.py
+++ BackEnd/Login_Auth.py
@@ -14,7 +14,7 @@ connection_details = [server_name, server_admin, server_password, database]
 def LogIn():
     email = request.json['email']
     password = request.json['password']
-    sql = SQL('localhost','root', '@dentista1')
+    sql = SQL(server_name,server_admin, server_password)
     condition = "email = '" +email + "' and password = '" + password + "'"
     result = sql.select_query(table = 'LOGIN_DATA', columns=['AccountType'], sql_condition=condition)
     sql.close_connection()
@@ -25,7 +25,7 @@ def LogIn():
 def GetName():
     email = request.json['email']
     AccountType = request.json['AccountType']
-    sql = SQL('localhost','root', '@dentista1')
+    sql = SQL(server_name,server_admin, server_password)
     if AccountType == 'Dentist':
         condition = "DENTIST_EMAIL = '" +email +  "'"
         result = sql.select_query(table = 'DENTIST', columns=['DENTIST_Fname', 'DENTIST_LNAME'], sql_condition=condition)
